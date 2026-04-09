df9937861ed0ec581f22cf828ee185c3e9de94af
Natinou.dev <155118603+Natinou12@users.noreply.github.com>
2026-04-09 19:14:12 -0300

    tabuada

--- Aulas/aula14ex/ex018-meu/scripts/script.js
+++ Aulas/aula14ex/ex018-meu/scripts/script.js
@@ -6,11 +6,19 @@ function calc() {
     var num = document.querySelector('#num')
     var tab = document.querySelector('#tab')
 
-    
+    // Valores
+
+    var vnum = Number(num.value)
+
+    // Limpar
+
+    tab.innerHTML = ''
 
     // Repetições
 
-    for (d = 1; d <= 10; d++) {
-        console.log(`${num} x ${d} = ${num * d}`)
+    for ( var d = 1; d <= 10; d++) {
+        tab.innerHTML += `${vnum} x ${d} = ${vnum * d} <br>`
     }
 }
+
+// Esse foi realmente mais fácil!
