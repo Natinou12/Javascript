e9888ce2e4e87ba844bc1db8a86d5fd6bb3e13ca
Natinou.dev <155118603+Natinou12@users.noreply.github.com>
2026-06-09 12:28:24 -0300

    atualização ex012

---
+++ Off-Aulas/ex012/ambiente.js
@@ -0,0 +1,10 @@
+const numeros = [16, 25, 144]
+
+let indiceAleatorio = Math.floor(Math.random() * numeros.length)
+
+let numeroSelecionado = numeros[indiceAleatorio]
+
+let raiz = Math.sqrt(numeroSelecionado)
+
+console.log(numeroSelecionado)
+console.log(raiz)
